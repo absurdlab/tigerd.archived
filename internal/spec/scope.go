064bb428ac3db2bb2371a2ab7a1578db4a12e474
Weinan Qiu <davidiamyou@gmail.com>
2022-12-10 15:44:23 +0800

    feat: add common scopes

---
+++ internal/spec/scope.go
@@ -0,0 +1,9 @@
+package spec
+
+const (
+	ScopeOpenID        = "openid"
+	ScopeOfflineAccess = "offline_access"
+	ScopeProfile       = "profile"
+	ScopeEmail         = "email"
+	ScopeAddress       = "address"
+)
